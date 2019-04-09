d79bf43e5372050b98354561f88c305ed6bc7fc9
denden <56295612@qq.com>
2019-04-09 19:25:36 +0800

    fix controling problem in "push" function

--- main.c
+++ main.c
@@ -4,19 +4,26 @@
 url:https://github.com/dendenxu/npl
 git command:git clone https://github.com/dendenxu/npl.git
 
-为了后期方便调试，在最后的几个版本中将stack.c stack.h main.c几个文件进行了合并，内容没有发生实际变动。
+为了后期方便调试，在最后的几个版本中将stack.c stack.h
+main.c几个文件进行了合并，内容没有发生实际变动。
 
 此项目
 1. 通过将用户输入的中缀表达式转换为后缀表达式
 2. 计算后缀表达式的值来实现简单的计算器功能
 
 注意：
-1. 一切计算都是在假定用户输入格式正确的情况下进行的，如果输入的格式不是严格的中缀表达式，谁知道会发生什么
-2. 支持的运算符：+、 -、 *、 /、 =       =代表运算的结束以及     （左括号     和    ）右括号
+1.
+一切计算都是在假定用户输入格式正确的情况下进行的，如果输入的格式不是严格的中缀表达式，谁知道会发生什么
+2. 支持的运算符：+、 -、 *、 /、 =       =代表运算的结束以及     （左括号     和
+）右括号
 3. 支持的数据类型：双精度浮点类型（注意不可以将0.3写成.3，但是可以将3.0写成3.）
-4. 栈大小：1000*sizeof(char)，浮点数组大小：1000*sizeof(double)，用户需保证输入的计算式不会超出两者的限制，否则谁知道会发生什么
-5. 用户输入的数据中可以在任意地方放入空白字符，比如用户输入1+1=或者1 + 1 = 都可以得到正确的结果（当然，不可以在原本应连续输入的函数中插入奇怪的空白字符，例如s i n）
-6. 所有运算都是浮点运算，所以最终结果可能与实际结果有微小的差异，但这也是不可避免的……，最终的运算结果输出形式与最终结果有关，有可能为E型或浮点型
+4.
+栈大小：1000*sizeof(char)，浮点数组大小：1000*sizeof(double)，用户需保证输入的计算式不会超出两者的限制，否则谁知道会发生什么
+5. 用户输入的数据中可以在任意地方放入空白字符，比如用户输入1+1=或者1 + 1 =
+都可以得到正确的结果（当然，不可以在原本应连续输入的函数中插入奇怪的空白字符，例如s
+i n）
+6.
+所有运算都是浮点运算，所以最终结果可能与实际结果有微小的差异，但这也是不可避免的……，最终的运算结果输出形式与最终结果有关，有可能为E型或浮点型
 
 项目分工：
 - 架构设计及实现：徐震
@@ -67,7 +74,8 @@ git command:git clone https://github.com/dendenxu/npl.git
 
 - hhh行吧我测试完了，修改了……hin多hin多bug，包括但不限于
 
-  - 调整了stdin的清空方式（用fflush不知道为啥总有些奇奇怪怪的错误，劳资直接用宏了）
+  -
+调整了stdin的清空方式（用fflush不知道为啥总有些奇奇怪怪的错误，劳资直接用宏了）
 
   - 读入无法识别函数的错误处理机制
   - 读入无法识别字符的错误处理机制
@@ -80,10 +88,11 @@ git command:git clone https://github.com/dendenxu/npl.git
 - 其实叭好像仍旧需要更多的测试点进行功能检验
 */
 
-#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXSTACKSIZE 10000 //用于创建栈和double数组
 
@@ -102,9 +111,10 @@ typedef struct _Stack
 
 Stack create(void);             //初始化栈（array和top都变为零）
 char pop(Stack stack);          //栈为空时返回'0'
-char push(Stack stack, char c); //达到最大值后报错并结束程序
+void push(Stack stack, char c); //达到最大值后报错并结束程序
 char peek(Stack stack);         //栈为空时返回'0'
-void destroy(Stack stack);      //清空该栈的内容（其实没啥卵用因为C里面没有对象或者析构函数）
+void destroy(
+    Stack stack); //清空该栈的内容（其实没啥卵用因为C里面没有对象或者析构函数）
 char peekmid(Stack stack, int index);
 double npl(void);
 
@@ -141,7 +151,7 @@ char pop(Stack stack)
     }
 }
 
-char push(Stack stack, char c)
+void push(Stack stack, char c)
 {
     if (stack->top == MAXSTACKSIZE - 1)
     {
@@ -149,9 +159,7 @@ char push(Stack stack, char c)
         exit(1);
     }
     else
-    {
         stack->array[stack->top++] = c;
-    }
 }
 
 char peek(Stack stack)
@@ -167,7 +175,7 @@ char peek(Stack stack)
 void destroy(Stack stack)
 {
     free(stack);
-    stack == NULL;
+    stack = NULL;
 }
 
 char peekmid(Stack stack, int index)
@@ -224,7 +232,10 @@ double func()
         //对于pow这种函数，有两个参数，不会出现后面不加括号的情况
         if (!strcmp(temp, "pow"))
         {
-            double temp1 = tempNum, temp2 = npl(); //最好不要直接在pow函数中调用两个npl，执行顺序是个变数。
+            double
+                temp1 = tempNum,
+                temp2 =
+                    npl(); //最好不要直接在pow函数中调用两个npl，执行顺序是个变数。
             return pow(temp1, temp2);
         }
         else if (!strcmp(temp, "sin"))
@@ -234,7 +245,7 @@ double func()
         else if (!strcmp(temp, "sqrt"))
             return sqrt(tempNum);
         else if (!strcmp(temp, "abs"))
-            return abs(tempNum);
+            return fabs(tempNum);
         else if (!strcmp(temp, "tan"))
             return tan(tempNum);
         else if (!strcmp(temp, "atan"))
@@ -263,8 +274,8 @@ double npl()
 {
     Stack infix = create();
     Stack postfix = create();
-    double num[MAXSTACKSIZE] = {0}; //num数组用于储存读入的数字
-    int cnt = 0;                    //cnt代表num的index
+    double num[MAXSTACKSIZE] = {0}; // num数组用于储存读入的数字
+    int cnt = 0;                    // cnt代表num的index
 
     //读入数据并直接转换为后缀表达式
     char ch;
@@ -289,19 +300,25 @@ double npl()
             case '*':
             case '/':
             case '(':
-                push(infix, ch); //低优先级的运算符直接压栈到infix
-                flag2 = 1;       //flag2用于判断是否处于合法的括号当中（便于递归函数的实现）
+                push(infix,
+                     ch); //低优先级的运算符直接压栈到infix
+                flag2 =
+                    1; // flag2用于判断是否处于合法的括号当中（便于递归函数的实现）
                 break;
             case '+':
             case '-':
                 while (peek(infix) == '*' || peek(infix) == '/')
-                    push(postfix, pop(infix)); //高优先级的运算符先将infix中低优先级的出栈到postfix再压栈到infix
+                    push(
+                        postfix,
+                        pop(infix)); //高优先级的运算符先将infix中低优先级的出栈到postfix再压栈到infix
                 push(infix, ch);
                 break;
             case ')':
                 if (flag2)
                 {
-                    while (peek(infix) != '(') //将infix中的运算符出栈，直到遇到'('，注意'('也要出栈，但不进入postfix
+                    while (
+                        peek(infix) !=
+                        '(') //将infix中的运算符出栈，直到遇到'('，注意'('也要出栈，但不进入postfix
                         push(postfix, pop(infix));
                     pop(infix);
                     flag2 = 0;
@@ -320,8 +337,10 @@ double npl()
             case '9':
             case '0':
                 ungetc(ch, stdin);
-                scanf("%lf", &num[cnt++]); //利用一个额外的数组进行数字的储存，以便直接利用scanf读入double类型功能的实现，cnt是该数组的指针（也可以用栈直接进行实现）
-                push(postfix, -cnt + 1);   //遇到数字直接进postfix
+                scanf(
+                    "%lf",
+                    &num[cnt++]);        //利用一个额外的数组进行数字的储存，以便直接利用scanf读入double类型功能的实现，cnt是该数组的指针（也可以用栈直接进行实现）
+                push(postfix, -cnt + 1); //遇到数字直接进postfix
                 break;
 
             case ',':
@@ -357,7 +376,8 @@ double npl()
             push(infix, ch);
             continue;
         }
-        switch (ch) //对postfix加减乘除直接进行运算，并将运算结果压栈到infix
+        switch (
+            ch) //对postfix加减乘除直接进行运算，并将运算结果压栈到infix
         {
         case '+':
             re = num[-pop(infix)] + num[-pop(infix)];
